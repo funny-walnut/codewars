aae00b307a3e4dfe4488854078b609badde49d94
funny-walnut <leraiztlt@gmail.com>
2023-08-07 23:13:31 +0400

    add riceAndChessboardProblem.js

---
+++ 7kyu/HeWheat/riceAndChessboardProblem.js
@@ -0,0 +1,21 @@
+// I assume most of you are familiar with the ancient legend of the rice (but I see wikipedia suggests wheat, for some reason) problem, but a quick recap for you: a young man asks as a compensation only 1 grain of rice for the first square, 2 grains for the second, 4 for the third, 8 for the fourth and so on, always doubling the previous.
+//
+//     Your task is pretty straightforward (but not necessarily easy): given an amount of grains, you need to return up to which square of the chessboard one should count in order to get at least as many.
+//
+//     As usual, a few examples might be way better than thousands of words from me:
+//
+//     squaresNeeded(0) === 0
+// squaresNeeded(1) === 1
+// squaresNeeded(2) === 2
+// squaresNeeded(3) === 2
+// squaresNeeded(4) === 3
+
+function squaresNeeded(grains){
+    if (grains === 0) return 0;
+
+    return Math.trunc((Math.log(grains) / Math.log(2))) + 1;
+}
+
+console.log(squaresNeeded(0));// 0
+console.log(squaresNeeded(3)); //2
+console.log(squaresNeeded(16)); //5
